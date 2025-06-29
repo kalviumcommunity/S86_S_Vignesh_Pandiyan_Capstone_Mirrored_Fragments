46a08173206513afda280dee503fb23d45514d28
Cheonyeoun <vigneshpandiyan08@gmail.com>
2025-06-30 03:58:11 +0530

    Implemented DB read/write using Mongoose

--- server/server.js
+++ server/server.js
@@ -11,6 +11,14 @@ app.use('/api/stats',playerStats);
 
 const PORT = process.env.PORT  || 3000
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("DB Connection Error:", err));
+
+
 app.get('/',(req,res)=>{
     return res.send({message:"Welcome To Mirrored Fragments Server!"});
 })
